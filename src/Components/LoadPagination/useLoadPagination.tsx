b9d0169a355dc1b6620a89611e9db58607abcc96
Kamikadze-Prog <kamikadze19900@gmail.com>
2021-03-10 21:02:37 +0200

    fixed loadPaginationHook

--- src/Components/LoadPagination/useLoadPagination.tsx
+++ src/Components/LoadPagination/useLoadPagination.tsx
@@ -1,24 +1,8 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {getStudents, getUniversities} from "../../Helpers/api";
-
-const divStyle = {
-    color: 'blue',
-    height: '40px',
-    textAlign: 'center',
-    padding: '3px 10px',
-    background: '#eee',
-    marginTop: '15px'
-};
-const containerStyle = {
-    maxWidth: '500px',
-    maxHeight: '300px',
-    margin: '0 auto',
-    overflow: 'auto',
-    border: '1px solid black'
-}
+import React, {useEffect, useState} from 'react';
 
 const useLoadPagination = <T extends unknown>(request: (count: number, offset: number) => Promise<T[]>, deps: React.DependencyList) => {
-    let count = 5;
+    let count = 10;
+    let unmounted = false;
     const [offset, setOffset] = useState(0);
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<Error | null>(null)
@@ -26,75 +10,43 @@ const useLoadPagination = <T extends unknown>(request: (count: number, offset: n
     const [hasMore, setHasMore] = useState(false);
     const [dispatchIndex, setDispatchIndex] = useState(0);
 
-    useEffect(() => {
-        setOffset(0);
-        setDispatchIndex(0);
-    }, [deps]);
+    // useEffect(() => {
+    //     console.log(  "deps" + deps + "|" + deps.length)
+    //      setOffset(0);
+    //     setDispatchIndex(0);
+    // }, [deps]);
 
     useEffect(() => {
         setLoading(true)
         setError(null);
-        let unmounted = false;
+        console.log(unmounted)
         request(count, offset).then(res => {
-            if (!unmounted)
+            if (unmounted)
                 return;
-
             setItems(prevList => {
                 return [...prevList, ...res]
             })
-            
             setHasMore(res.length >= count)
             setLoading(false)
             setOffset(offset + count);
-            //console.log(offset + " offset: "+ count + ": count  "+ page + ": page");
-        }).catch(setError);
+            console.log(res);
+            console.log(offset + " offset: " + count + ": dispatchIndex" +dispatchIndex + ": page");
+        }).catch();
         return () => {
             unmounted = true;
         }
-    }, [ dispatchIndex ]);
+    }, [dispatchIndex]);
 
     return {
         loading,
         error,
+        hasMore,
         items,
         dispatch: () => {
             if (!loading)
                 setDispatchIndex(i => i + 1);
         }
     };
-
-    // const showItems = () => {
-    //     return (
-    //         <React.Fragment>
-    //             <div style= {containerStyle}>
-    //                 {items.map((element: any, index)  => {
-    //                     const {name} = element;
-    //                     if (items.length === index + 1) {
-    //                         return (
-    //                             <div ref={lastListElementRef} style= {divStyle}
-    //                                  key={index}>
-    //                                 {name}
-    //                             </div>
-    //                         );}
-    //                     else {
-    //                         
-    //                         return <div   style= {divStyle} key={index}>{name}</div>
-    //                     }
-    //                 })}
-    //                 <div>{loading && 'Loading...'}</div>
-    //                 <div>{error && 'Error'}</div>
-    //             </div>
-    //         </React.Fragment>
-    //     );
-    // };
-    //
-    //     return (
-    //     <React.Fragment>
-    //         <div className='container'>
-    //             {showItems}
-    //         </div>
-    //     </React.Fragment>
-    // );
 };
 
 export default useLoadPagination;
--- src/Components/SignUpPage/RegistrationForm.tsx
+++ src/Components/SignUpPage/RegistrationForm.tsx
@@ -1,57 +1,54 @@
-import React from "react";
-import {DataSource, getRegions, getStudents} from "../../Helpers/api";
+import React, {useCallback, useRef, useState} from "react";
+import {DataSource, getRegions, getStudents, getUniversities} from "../../Helpers/api";
 import useLoadPagination from "../LoadPagination/useLoadPagination";
+import LoadingSpinner from "../LoadingSpinner";
 
 export default function RegistrationForm() {
-    const divStyle = {
-        color: 'blue',
-        height: '40px',
-        textAlign: 'center',
-        padding: '3px 10px',
-        background: '#eee',
-        marginTop: '15px'
-    };
-    const containerStyle = {
-        maxWidth: '500px',
-        maxHeight: '300px',
-        margin: '0 auto',
-        overflow: 'auto',
-        border: '1px solid black'
-    }
+    const divStyle = {color: 'blue', height: '40px', textAlign: 'center', padding: '3px 10px', background: '#eee', marginTop: '15px'
+    } as React.CSSProperties;
+    const containerStyle = {maxWidth: '500px', maxHeight: '300px', margin: '0 auto', overflow: 'auto', border: '1px solid black'
+    } as React.CSSProperties;
+    const [query, setQuery] = useState('');
 
-    const {
-        loading, error, items, dispatch
-    } = useLoadPagination((count, offset) => getStudents(count, offset), []);
+    const {loading, hasMore, error, items, dispatch
+    } = useLoadPagination(
+        React.useCallback((count, offset) => getUniversities(query, 1, count, offset), [1]),
+        [query])
+
+    const observer = useRef<IntersectionObserver>()
+    const lastListElementRef = useCallback(node => {
+        if (observer.current) {observer.current.disconnect()}
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting ) {dispatch()}})
+        if (node) {observer.current.observe(node)}
+    }, [loading])
 
     const showItems = () => {
-        return (// @ts-ignore
+        return (
             <React.Fragment>
-                <div style= {containerStyle}>
+                <div style= {containerStyle} >
                     {items.map((element: any, index)  => {
                         const {name} = element;
-                        if (items.length === index + 1) {
-                            return (// @ts-ignore
-                                <div style= {divStyle}
+                        if (items.length === index + 1 && hasMore) {
+                            return (
+                                <div style= {divStyle} ref={lastListElementRef}
                                      key={index}>
                                     {name}
                                 </div>
                             );}
                         else {
-                            // @ts-ignore
-                            return <div   style= {divStyle} key={index}>{name}</div>
+                            return <div style= {divStyle} key={index}>{name}</div>
                         }
                     })}
-                    <div>{loading && 'Loading...'}</div>
+                    <div>{loading && <LoadingSpinner/>}</div>
                     <div>{error && 'Error'}</div>
                 </div>
             </React.Fragment>
         );
     };
 
-
     return <div className={`SignForm`}>
         {showItems()}
-
         <div className={`SureNameForm`}>
             <input type={`text`} placeholder={`Ім’я`}/>
             <input type={`text`} placeholder={'Прізвище'}/>
