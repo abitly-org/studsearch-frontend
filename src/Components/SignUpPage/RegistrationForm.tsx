c63b23b89ac900ff2a54d79cb01333e167e8840d
Kamikadze-Prog <kamikadze19900@gmail.com>
2021-03-13 16:35:18 +0200

    fixed scss style for registration page

--- src/Components/SignUpPage/RegistrationForm.tsx
+++ src/Components/SignUpPage/RegistrationForm.tsx
@@ -1,61 +1,16 @@
-import React, {useCallback, useRef, useState} from "react";
+import React, {useState} from "react";
 import tgPhoto from "./tgPhoto.svg"
-import {DataSource, getRegions, getStudents, getUniversities} from "../../Helpers/api";
-import useLoadPagination from "../LoadPagination/useLoadPagination";
-import LoadingSpinner from "../LoadingSpinner";
 
 export default function RegistrationForm() {
-    const divStyle = {
-        color: 'blue', height: '40px', textAlign: 'center', padding: '3px 10px', background: '#eee', marginTop: '15px'
-    } as React.CSSProperties;
-    const containerStyle = {
-        maxWidth: '500px', maxHeight: '300px', margin: '0 auto', overflow: 'auto', border: '1px solid black'
-    } as React.CSSProperties;
-    const [query, setQuery] = useState('');
 
-    const {loading, hasMore, error, items, dispatch
-    } = useLoadPagination(
-        React.useCallback((count, offset) => getUniversities(query, 1, count, offset), [1]),
-        [query])
-
-    const observer = useRef<IntersectionObserver>()
-    const lastListElementRef = useCallback(node => {
-        if (observer.current) {observer.current.disconnect()}
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting ) {dispatch()}})
-        if (node) {observer.current.observe(node)}
-    }, [loading])
-
-    const showItems = () => {
-        return (
-            <React.Fragment>
-                <div style= {containerStyle} >
-                    {items.map((element: any, index)  => {
-                        const {name} = element;
-                        if (items.length === index + 1 && hasMore) {
-                            return (
-                                <div style= {divStyle} ref={lastListElementRef}
-                                     key={index}>
-                                    {name}
-                                </div>
-                            );}
-                        else {
-                            return <div style= {divStyle} key={index}>{name}</div>
-                        }
-                    })}
-                    <div>{loading && <LoadingSpinner/>}</div>
-                    <div>{error && 'Error'}</div>
-                </div>
-            </React.Fragment>
-        );
-    };
+    const [tg, setTg] = useState(false);
+    const [politic, setPolitic] = useState(false);
+    const checkboxTg = ((event: React.ChangeEvent<HTMLInputElement>) => setTg(event.target.checked));
+    const checkboxPolitic = ((event: React.ChangeEvent<HTMLInputElement>) => setPolitic(event.target.checked));
 
     return <div className={`SignForm`}>
-        {/*{showItems()}*/}
         <div className={`FormBox`}>
-
         </div>
-
         <div className="InputBox">
             <div className="Input">
                 <span>Про себе</span>
@@ -67,13 +22,13 @@ export default function RegistrationForm() {
         </div>
 
         <div className="TgPhoto">
-            <input type="checkbox" name="tgPhoto"/>
+            <input type="checkbox" checked={tg} onChange={checkboxTg} name="tgPhoto"/>
             <label htmlFor="tgPhoto">Додати моє фото з Telegram
             </label>
         </div>
 
         <div className="Politic">
-            <input type="checkbox" name="politic"/>
+            <input type="checkbox" checked={politic} onChange={checkboxPolitic} name="politic"/>
             <label htmlFor="politic">Погоджуюся з
                 <a href={`#`}>Політикою конфіденційності</a>
             </label>
@@ -82,8 +37,8 @@ export default function RegistrationForm() {
             тому просимо тебе підтвердити свій аккаунт через Telegram-бота</p>
 
         <div className="authTelegram">
-            <img src={tgPhoto}/>
-            <a href={`#`}><span>Підтвердити Telegram та зареєструватись</span></a>
+
+            <a href={`#`}><img src={tgPhoto} alt="tgPhoto"/><span>Підтвердити Telegram та зареєструватись</span></a>
         </div>
         <div className={`goBack`}>
             <a className="goBack-btn" href="/">Назад</a>
