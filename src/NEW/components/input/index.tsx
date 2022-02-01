cda052c345514745c20ad6d4401423f161179699
vlados407 <vladsurin407@gmail.com>
2022-02-01 14:26:15 +0200

    add validation to input

--- src/NEW/components/input/index.tsx
+++ src/NEW/components/input/index.tsx
@@ -12,22 +12,38 @@ const Input = ({
   name?: string,
   value?: string,
   onChange?: (newValue: string) => void
-}) => (
-  <div className={cx('AppInput', { filled: !!value })}>
-    <div className='Placeholder'>{name}</div>
-    <input
-      value={value}
-      onChange={e => onChange?.(e?.target?.value)}
-    />
-    { value && 
-      <span
-        className='AppInputClear'
-        onClick={() => onChange?.('')}
-      >
-        <img src={close} />
-      </span>
-    }
-  </div>
-);
+}) => {
+  React.useEffect(() => {
+    const handleEsc = (event: any) => {
+      if (event.keyCode === 27)  {
+        onChange?.('')
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [])
+
+  return (
+    <div className={cx('AppInput', { filled: !!value })}>
+      <div className='Placeholder'>{name}</div>
+      <input
+        maxLength={3} 
+        value={value}
+        onChange={(e) => (e?.target?.value.replace(/\D/g, '') && e?.target?.value <= '200' ? onChange?.(e?.target?.value) : onChange?.(''))}
+      />
+      { value && 
+        <span
+          className='AppInputClear'
+          onClick={() => onChange?.('')}
+        >
+          <img src={close} />
+        </span>
+      }
+    </div>
+  )
+};
 
 export default Input;
